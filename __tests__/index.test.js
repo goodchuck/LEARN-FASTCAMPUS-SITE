eea4254fc58c5647e435ac3f724b2b17aaa42b50
yang <y502100@naver.com>
2023-09-26 17:52:22 +0900

    CH04-03. 페이지 생성하기

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,55 @@
-import { render, screen } from '@testing-library/react'
+import { getByTitle, render, screen } from '@testing-library/react'
 import Home from '../src/app/page'
 import '@testing-library/jest-dom'
 
-describe('Home', () => {
-    it('renders a heading', () => {
-        render(<Home />)
+// describe('App', () => {
+//     it('메뉴가 렌더링 되어야 한다', () => {
+//         const { getByRole } = render (<Index />);
 
-        const heading = screen.getByRole('heading', {
-            name: /welcome to next\.js!/i,
+//         const menu = getByRole('nav', {
+//             name : /fastcampus/g,
+//         })
+
+//         expectmenu(menu).toBeInTheDocument();
+//     })
+// })
+
+// CH04-03.페이지 생성하기 테스트 케이스는 당장은 깨져도 되지만 테스트 기반 코딩을 하는게 중요하다.
+describe('Home', () => {
+    it('메뉴가 렌더링 되어야 한다', () => {
+        const { getByRole } = render (<Home />);
+
+        const menu = getByRole('navigation', {
+            name : 'fastcampus',
         })
 
-        expect(heading).toBeInTheDocument()
+        expect(menu).toBeInTheDocument();
     })
+
+    it('배너가 렌더링 되어야 한다', () => {
+        const { getByRole } = render (<Home />);
+
+        const banner = getByRole('banner')
+
+        expect(banner).toBeInTheDocument();
+    })
+
+    it('강의목록이 렌더링 되어야 한다', () => {
+        const { getByTitle } = render (<Home />);
+
+        const lectureList = getByTitle('lectureList')
+
+        expect(lectureList).toBeInTheDocument();
+    })
+
+
+    // it('renders a heading', () => {
+    //     render(<Home />)
+
+    //     const heading = screen.getByRole('heading', {
+    //         name: /welcome to next\.js!/i,
+    //     })
+
+    //     expect(heading).toBeInTheDocument()
+    // })
 })
--- src/app/detail.tsx
+++ src/app/detail.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function Detail() {
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
